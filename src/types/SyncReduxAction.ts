c595138a80a4829b94ea52bebe96c6d52f4e6158
neloreck <Neloreck@gmail.com>
2018-09-06 00:18:40 +0300

    Include d.ts for typings.

--- src/types/SyncReduxAction.ts
+++ src/types/SyncReduxAction.ts
@@ -2,14 +2,14 @@ import {Action} from "redux";
 
 export abstract class SyncReduxAction implements Action {
 
-  private static readonly type: string;
+  public static readonly type: string;
   public readonly type: string;
 
   public constructor() {
     this.type = this.getActionType();
   }
 
-  protected getActionType(): string {
+  public getActionType(): string {
     return Object.getPrototypeOf(this).type;
   }
 
--- src/utils/createAsyncActionType.ts
+++ src/utils/createAsyncActionType.ts
@@ -4,8 +4,8 @@ import {SyncReduxAction} from "../";
 export function createAsyncActionType(dispatch: Dispatch) {
   return class AsyncReduxAction extends SyncReduxAction {
 
-    protected dispatch<T extends SyncReduxAction>(action: T): T {
-      return dispatch(action);
+    public dispatch<T extends SyncReduxAction>(action: T): T {
+       return dispatch(action);
     }
 
   }
